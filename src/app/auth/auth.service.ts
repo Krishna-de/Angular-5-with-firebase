5f138f8a2db7f93758635c1f4bbc98eeb21a534f
Krishna-de <gsrk.de@gmail.com>
2018-01-16 00:44:20 +0100

    implement authservice

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,14 +7,14 @@ export class AuthService {
   token: string;
   constructor(private router: Router){}
 
-    signupUser(email: string, password: string) {
-        firebase.auth().createUserWithEmailAndPassword(email, password)
+  signupUser(email: string, password: string) {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
           .catch(
             error => console.log(error)
           )
       }
-      signinUser(email: string, password: string) {
-        firebase.auth().signInWithEmailAndPassword(email, password)
+  signinUser(email: string, password: string) {
+      firebase.auth().signInWithEmailAndPassword(email, password)
           .then(
             response => {
               this.router.navigate(['/']);
@@ -28,19 +28,18 @@ export class AuthService {
             error => console.log(error)
           );
       }
-      logout(){
-        firebase.auth().signOut();
+  logout() {
+      firebase.auth().signOut();
         this.token = null;
       }
-      getToken() {
-        firebase.auth().currentUser.getToken()
+  getToken() {
+      firebase.auth().currentUser.getToken()
         .then(
           (token: string) => this.token = token
         );
         return this.token;
       }
-
-      isAuthenticated(){
+  isAuthenticated(){
         return this.token != null;
       }
 }
